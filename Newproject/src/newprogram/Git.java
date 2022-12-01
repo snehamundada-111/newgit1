72b3e4e55f6e9e25e5a8a9f4191d337c01429442
snehamundada-111 <snehamundada2012@gmail.com>
2022-11-30 22:45:57 -0500

    hii
    my first program

---
+++ Newproject/src/newprogram/Git.java
@@ -0,0 +1,11 @@
+package newprogram;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to gitub world");
+
+	}
+
+}
